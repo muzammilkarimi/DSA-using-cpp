2f710ee4f95d3d8891b93c2dd0421d36a4ac7176
Muzammil Ahmad Karimi <72164898+muzammilkarimi@users.noreply.github.com>
2025-06-29 21:52:26 +0530

    test

--- TLE/first.cpp
+++ TLE/first.cpp
@@ -1,6 +1,9 @@
-##include<bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 int main(){
-    
+    int n;
+    cin>>n; 
+    vector<int> arr(n);
+    for(auto &it:arr) cin>>it;
     return 0;
 }
